3c932dede78bcf9685d65c64a02983184a1d50d0
Gentleelephant <birdhk@kubesphere.io>
2022-10-12 00:54:15 +0800

    update log

--- pzlog/log.go
+++ pzlog/log.go
@@ -89,14 +89,14 @@ func setDefaultValue(config *PzlogConfig) {
 
 }
 
-func GinLogger(logger *zap.Logger) gin.HandlerFunc {
+func GinLogger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
 		query := c.Request.URL.RawQuery
 		c.Next()
 		cost := time.Since(start)
-		logger.Info(path,
+		zap.L().Info(path,
 			zap.Int("status", c.Writer.Status()),
 			zap.String("method", c.Request.Method),
 			zap.String("path", path),
